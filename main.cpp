64a7e0e9e6416cadd1f2238ae832411ee9aa2bb6
badtothebunny <maria.m.psomas@gmail.com>
2025-09-13 10:44:48 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -16,27 +16,27 @@ int main(int argc, char** argv)
 	EnglishDictionary test("Test");
 	cout<<"Next I create an Entry instance that has words with definitions, repititions being 3, 4, 1, and then 2."<<endl;
 	//3 repetitions	
-	Entry<string,string> t1("Kutrolli","An awesome professor");
-	Entry<string,string> t2("Kutrolli", "Will give Maria an A");
-	Entry<string,string> t3("Kutrolli", "Smart about C++");
+	Entry<string,string> t1("Professor","An awesome professor");
+	Entry<string,string> t2("Professor", "Will give Bunny an A");
+	Entry<string,string> t3("Professor", "Smart about C++");
 	test.add(t1);
 	test.add(t2);
 	test.add(t3);
 	//4 repetitions
-	Entry<string,string> t4("Maria","Will recieve an A in CSCI 373");
-	Entry<string,string> t5("Maria","Learning to program");
-	Entry<string,string> t6("Maria","Gets a 100 on the final project");
-	Entry<string,string> t7("Maria","Tries her hardest :)");
+	Entry<string,string> t4("Bunny","Will recieve an A in CSCI 373");
+	Entry<string,string> t5("Bunny","Learning to program");
+	Entry<string,string> t6("Bunny","Gets a 100 on the final project");
+	Entry<string,string> t7("Bunny","Tries their hardest :)");
 	test.add(t4);
 	test.add(t5);
 	test.add(t6);
 	test.add(t7);
 	////1 repetition
-	Entry<string,string> t8("False","Maria getting an F");
+	Entry<string,string> t8("False","Bunny getting an F");
 	test.add(t8);
 	//2 repetitions
-	Entry<string,string> t9("Fact","Maria got an A!");
-	Entry<string,string> t10("Fact","Maria is happy.");
+	Entry<string,string> t9("Fact","Bunny got an A!");
+	Entry<string,string> t10("Fact","Bunny is happy.");
 	test.add(t9);
 	test.add(t10);
 	cout<<"Printing all the words from the beginning."<<endl;
@@ -45,13 +45,14 @@ int main(int argc, char** argv)
 	test.printDictionary(false,test.uniqueWords());
 	cout<<"Printing the word with 2 definitions."<<endl;
 	test.printDictionary("Fact",true);
-	test.deleteFirst("Kutrolli");
-	test.printDictionary("Maria");
-	test.deleteFirst("Maria");
-	test.deletes("Kutrolli");
+	test.deleteFirst("Professor");
+	test.printDictionary("Bunny");
+	test.deleteFirst("Bunny");
+	test.deletes("Professor");
 	test.deletes("Fact");
 	test.printDictionary(true,test.uniqueWords());
 	test.printDictionary(false,test.uniqueWords());
 	return 0;
 }
+
 
